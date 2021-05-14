7e9d08185f19770f96f677e32c2bac35df7ab8c2
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-05-14 18:05:22 -0300

    ./

--- ShiftAndAP/ShiftAndAP.c
+++ ShiftAndAP/ShiftAndAP.c
@@ -2,53 +2,58 @@
 
 
 int ShiftAndAP(char *Text, char *Pattern, long TSize, long PSize, long k, short op){   
-    printf("Entrou aqui\n");
+    
     long match = 0; 
     long  Mask[MAXCHAR], i , j , Ri, Rant, Rnew;  
-    long R[MAXCHAR + 1];  
+    long R[NUMMAXERROS + 1];  
     for(i = 0 ; i < MAXCHAR; i++) 
-        Mask[i] = 0 ;  
+    {
+        Mask[i] = 0 ;   
+    }
     for(i = 1 ; i <= PSize; i++) 
-        Mask[Pattern[i+1] + 127] |=1 << (PSize-i); 
+    { 
+        Mask[Pattern[i-1] + 127] |=1 << (PSize-i); 
+    } 
     R[0] = 0;  
     Ri = 1 << (PSize -1); 
     for(j =1; j <= k ; j++) 
         R[j] = (1 << (PSize-j)) | R[j-1]; 
     
-    for(i = 0; i < TSize;i++){ 
+    for(i = 0; i < TSize; i++){ 
         Rant = R[0]; 
-        Rnew= ((((unsigned long) Rant) >> 1)| Ri ) &Mask[Text[i] + 127]; 
+        Rnew= ((((unsigned long) Rant) >> 1)| Ri ) & Mask[Text[i] + 127]; 
         R[0] = Rnew;   
         for(j = 1; j <= k; j++){  
             if(op == 1){  
-                printf("insercao\n");
                 //insercao
                 Rnew = ((((unsigned long)R[j]) >> 1) & Mask[Text[i] + 127]) | Rant ;   
             }
             if(op == 2){   
-                printf("remocao\n");
-                //remocao
-                Rnew = ((((unsigned long)R[j]) >> 1) & Mask[Text[i] + 127]) | (((unsigned long)Rnew) >> 1);  
+                //remocaoc
+                Rnew = ((((unsigned long)R[j]) >> 1) & Mask[Text[i] + 127]) | (((unsigned long)Rnew) >> 1);   
+               
             } 
             if(op == 3){   
-                printf("substituicao\n");
                 // substituicao   
                 Rnew = ((((unsigned long)R[j]) >> 1) & Mask[Text[i] + 127]) | (((unsigned long)Rant) >> 1);  
-              
+               
             }
             if(op == 4){ 
-                printf("Todas as operacoes\n");
                 Rnew = ((((unsigned long)R[j]) >> 1) & Mask[Text[i] + 127]) | Rant | (((unsigned long)(Rant | Rnew)) >> 1);
-            }
+                
+            } 
             Rant = R[j];
             R[j] = Rnew | Ri; 
+           
         } 
         if((Rnew & 1) != 0){ 
-            printf(" * Matching: %ld\n", i + 1); 
-            for(int ii = 0 ; ii < PSize ;ii++){  
-                ii = i + 1; 
-                printf("%c",Rnew[]);
-            } 
+            printf("* Matching: %ld", i + 1); 
+            for(int l = PSize ; l < 0 ;l--){  
+                int aux = i + 1;  
+                int index = aux - l; 
+                printf("%c",Text[index]);
+            }  
+            printf("\n");
             match +=1;  
         }
     } 
--- ShiftAndAP/ShiftAndAP.h
+++ ShiftAndAP/ShiftAndAP.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "../Util/Cadeia.h"
 #define MAXCHAR 256 
-
+#define NUMMAXERROS 10
 /*Functions*/ 
 
 int ShiftAndAP(char *Text, char *Pattern, long TSize, long PSize, long k, short op);
